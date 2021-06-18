0173c311720aefd2fa93ae3a3eca2dbbd208878e
SpreeHertz <spreehertzofficial@gmail.com>
2021-06-18 22:05:09 +0530

    Working on dashboad + still learning React

---
+++ dashboard/src/pages/Commands.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+/*
+will do this later
+*/
+
+export const Commands = () => (
+  <div>
+      <h1>Cleckzie's Commands</h1> 
+  </div>
+)
---
+++ dashboard/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const DashboardPage = () => (
+  <div>
+      <h1>Dashboard Page</h1>
+  </div>
+)
+
