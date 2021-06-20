146386c9752160f50c5154df1f7db0f528fd3724
SpreeHertz <spreehertzofficial@gmail.com>
2021-06-20 21:54:40 +0530

    addRole.js

--- commands/moderation/addRole.js
+++ commands/moderation/addRole.js
@@ -1,24 +1,36 @@
-const { Message } = require('discord.js')
+const { Message, MessageEmbed } = require('discord.js')
 
 module.exports = {
     name : 'addrole',
     description: "Gives a role to a user. **Please be careful with this command. If you give wrong roles to the wrong user, it can give serious consequences for the server. We also have a remove role feature, just in case if that happens.**",
     usage: "*addrole @Miikie @Tester",
     run : async(client, message, args) => {
-        //lets use parameters (optional)
         /**
          * @param {Message} message
-         */
-        //so firstly we will check whether the author of the message has permissions
-        //this line means if the author doesn't have manage roles permission it will stop the process and send the following text
+         */ 
         if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send('You do not have permission.')
-        //next we define some variables
-        const target = message.mentions.members.first() //member = mentions
-        if(!target) return message.channel.send(':x: No member specified.') //when no member is pinged
-        const role = message.mentions.roles.first() // roles = mentions
-        if(!role) return message.channel.send(':x: No role specified.') //when no role is specified or pinged
-        //now the code!
-        await target.roles.add(role) // adding the role to the user
+        const target = message.mentions.members.first() 
+        if(!target) {
+            const embed = new MessageEmbed()
+            .setTitle("Here's how to use this command")
+            .setDescription("`addrole {user} {role}`")
+            .setFooter('Do not literally type out {}.')
+            .setColor("GOLD")
+            .setAuthor(message.author.displayName, message.author.displayAvatarURL({ dynamic: true }))
+        } 
+
+        const role = message.mentions.roles.first() 
+        if(!role) {  
+            const noRoleEmbed = new MessageEmbed()
+            .setTitle("Here's how to use this command:")
+            .setDescription("`addrole {user} {role}`")
+            .setFooter('Do not literally type out {}.')
+            .setColor("GOLD")
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+            return message.channel.send(noRoleEmbed)
+        }
+        
+        await target.roles.add(role)
         message.channel.send(`${target.user.username} has obtained ${role} role.`)
     }
 }
