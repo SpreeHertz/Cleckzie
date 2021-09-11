2c219e06aba0f58f32888a81e5d3f54e9f33f09a
SpreeHertz <spreehertzofficial@gmail.com>
2021-09-11 21:51:52 +0530

    Enabled ESLint

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
-const client = require("../index");
+const client = require('../index');
 const chalk = require('chalk');
 
-client.on("ready", () =>
-    console.log(chalk.yellow(`${client.user.tag}`) + chalk.green(" is up and ready to go.") + chalk.cyan("\nYou will now be able to use") + chalk.green(" Slash Commands and messages."))
+client.on('ready', () =>
+	console.log(chalk.yellow(`${client.user.tag}`) + chalk.green(' is up and ready to go.') + chalk.cyan('\nYou will now be able to use') + chalk.green(' Slash Commands and messages.')),
 );
--- handler/index.js
+++ handler/index.js
@@ -1,6 +1,7 @@
-const { glob } = require("glob");
-const { promisify } = require("util");
-const { Client } = require("discord.js");
+const { glob } = require('glob');
+const { promisify } = require('util');
+// eslint-disable-next-line no-unused-vars
+const { Client } = require('discord.js');
 const globPromise = promisify(glob);
 
 
@@ -8,38 +9,38 @@ const globPromise = promisify(glob);
  * @param {Client} client
  */
 module.exports = async (client) => {
-    // Commands
-    const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
-    commandFiles.map((value) => {
-        const file = require(value);
-        const splitted = value.split("/");
-        const directory = splitted[splitted.length - 2];
+	// Commands
+	const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+	commandFiles.map((value) => {
+		const file = require(value);
+		const splitted = value.split('/');
+		const directory = splitted[splitted.length - 2];
 
-        if (file.name) {
-            const properties = { directory, ...file };
-            client.commands.set(file.name, properties);
-        }
-    });
+		if (file.name) {
+			const properties = { directory, ...file };
+			client.commands.set(file.name, properties);
+		}
+	});
 
-    // Events
-    const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
-    eventFiles.map((value) => require(value));
+	// Events
+	const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
+	eventFiles.map((value) => require(value));
 
-    // Slash Commands
-    const slashCommands = await globPromise(
-        `${process.cwd()}/SlashCommands/*/*.js`
-    );
+	// Slash Commands
+	const slashCommands = await globPromise(
+		`${process.cwd()}/SlashCommands/*/*.js`,
+	);
 
-    const arrayOfSlashCommands = [];
-    slashCommands.map((value) => {
-        const file = require(value);
-        if (!file?.name) return;
-        client.slashCommands.set(file.name, file);
+	const arrayOfSlashCommands = [];
+	slashCommands.map((value) => {
+		const file = require(value);
+		if (!file?.name) return;
+		client.slashCommands.set(file.name, file);
 
-        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
-        arrayOfSlashCommands.push(file);
-    });
-    client.on("ready", async () => {
-        await client.application.commands.set(arrayOfSlashCommands);
-    });
+		if (['MESSAGE', 'USER'].includes(file.type)) delete file.description;
+		arrayOfSlashCommands.push(file);
+	});
+	client.on('ready', async () => {
+		await client.application.commands.set(arrayOfSlashCommands);
+	});
 };
