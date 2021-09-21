d8bb5573db61fedc4a82f749a3980e30cbe0c399
SpreeHertz <spreehertzofficial@gmail.com>
2021-09-21 22:47:30 +0530

    Update x3

---
+++ commands/info/ping.js
@@ -0,0 +1,17 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    aliases: ['p'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        const embed = new MessageEmbed()
+        .addField('Websocket', `${client.ws.ping}ms`)
+        message.channel.send({embeds: [embed]})
+    },
+};
---
+++ handler/index.js
@@ -0,0 +1,60 @@
+const { glob } = require("glob");
+const { promisify } = require("util");
+const { Client } = require("discord.js");
+const { mongooseConnectionString } = require("../config.json");
+const mongoose = require("mongoose");
+
+const globPromise = promisify(glob);
+
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+    // Commands
+    const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+    commandFiles.map((value) => {
+        const file = require(value);
+        const splitted = value.split("/");
+        const directory = splitted[splitted.length - 2];
+
+        if (file.name) {
+            const properties = { directory, ...file };
+            client.commands.set(file.name, properties);
+        }
+    });
+
+    // Events
+    const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
+    eventFiles.map((value) => require(value));
+
+    // Slash Commands
+    const slashCommands = await globPromise(
+        `${process.cwd()}/SlashCommands/*/*.js`
+    );
+
+    const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        client.slashCommands.set(file.name, file);
+
+        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+        arrayOfSlashCommands.push(file);
+    });
+    client.on("ready", async () => {
+      
+        await client.application.commands.set(arrayOfSlashCommands);
+
+     
+    });
+
+    // mongoose
+    if (!mongooseConnectionString) return;
+
+    mongoose.connect(mongooseConnectionString, {
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+    });
+};
+
+// test
