1ba032366ffae3d16223a97c459409b4a35ea570
SpreeHertz <spreehertzofficial@gmail.com>
2021-09-22 15:00:54 +0530

    Fix errors

--- SlashCommands/info/ping.js
+++ SlashCommands/info/ping.js
@@ -1,16 +1,25 @@
-const { Client, CommandInteraction } = require("discord.js");
+/* eslint-disable no-unused-vars */
+const { Client, CommandInteraction, MessageEmbed, User, Message } = require("discord.js");
 
 module.exports = {
-    name: "ping",
-    description: "returns websocket ping",
-    type: 'CHAT_INPUT',
-    /**
+	name: "ping",
+	description: "Returns websocket ping",
+	type: 'CHAT_INPUT',
+	/**
      *
      * @param {Client} client
      * @param {CommandInteraction} interaction
      * @param {String[]} args
      */
-    run: async (client, interaction, args) => {
-        interaction.followUp({ content: `${client.ws.ping}ms!` });
-    },
+	run: async (client, interaction, args, message) => {
+		if (message) {
+			const pingEmbed = new MessageEmbed()
+				.setColor('RANDOM')
+				.addField('Websocket', `${client.ws.ping}`)
+				.setTimestamp()
+				.setAuthor(User.name, User.displayAvatarURL({ dynamic: true }));
+			interaction.followUp({ embeds: [pingEmbed] });
+		}
+
+	},
 };
--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,16 +3,29 @@ const { Message, Client, MessageEmbed } = require("discord.js");
 
 module.exports = {
 	name: "ping",
-	aliases: ['p'],
 	/**
      *
      * @param {Client} client
      * @param {Message} message
      * @param {String[]} args
      */
-	run: async (client, message, args) => {
+	run: async (client, message) => {
+		// roleColor
+		const roleColor =
+          message.guild.me.displayHexColor === "#000000" ? "#ffffff" : message.guild.me.displayHexColor;
+		// Circles
+		const circles = {
+			green: "🟢",
+			yellow: "🟡",
+			red: "🔴",
+		};
+
 		const embed = new MessageEmbed()
-			.addField('Websocket', `${client.ws.ping}ms`);
+			.setColor(roleColor)
+			.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+			.addField("Websocket",
+				`${client.ws.ping <= 200 ? circles.green : client.ws.ping <= 400 ? circles.yellow : circles.red} ${client.ws.ping}ms`,
+			);
 		message.channel.send({ embeds: [embed] });
 	},
 };
