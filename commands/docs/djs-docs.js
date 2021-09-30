10c4c2aac298955fab768f4e9fa27fc3d83e40cc
SpreeHertz <spreehertzofficial@gmail.com>
2021-09-30 12:35:37 +0530

    New files

---
+++ commands/docs/djs-docs.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+module.exports = {
+	name: "djs",
+	aliases: ["docs"],
+	/**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+	run: async (client, message, args) => {
+		const query = args.join(" ");
+		if (!query) return message.reply("Please specify a query!");
+		const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
+			query,
+		)}`;
+
+		axios.get(url).then(({ data }) => {
+			if (data) {
+				message.channel.send({ embed: data });
+			}
+		});
+	},
+};
