e10240c8a3d10e2adb578d99fe3c4800105a89d4
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-02 23:04:36 +0530

    ping.js fix

--- SlashCommands/info/ping.js
+++ SlashCommands/info/ping.js
@@ -17,7 +17,7 @@ module.exports = {
 			.setColor('RANDOM')
 			.addField('Websocket', `${client.ws.ping}`)
 			.setTimestamp()
-			.setAuthor(User.name, interaction.user.displayAvatarURL({ dynamic: true }));
+			.setAuthor(interaction.user.name, interaction.user.displayAvatarURL({ dynamic: true }));
 		interaction.reply({ embeds: [pingEmbed] }).then(console.log(chalk.cyan(` ${interaction.user.username}`) + chalk.green(' executed') + chalk.cyan(' SlashCommands/ping.js') + chalk.green(' in the channel') + chalk.cyan(` #${interaction.channel.name}`) + chalk.green(' on the guild') + chalk.cyan(` ${interaction.guild}.`)));
 
 
