22d7d2bf63299a44f975802502d3fd2fb265c397
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-08 15:29:59 +0530

    dashboard implementation [In dev phase, don't use]

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-inline-comments */
+module.exports = {
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
