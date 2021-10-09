9871a64c18fffee87c6861ce8af1c016fe454ff4
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-09 23:25:07 +0530

    Implement discord-xp

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Collection } = require("discord.js");
 require('dotenv').config();
+const chalk = require('chalk');
 
 // Note: 32767 means all intents.
 const client = new Client({
@@ -15,5 +16,29 @@ client.slashCommands = new Collection();
 client.config = require("./config/config.json");
 
 require("./handler")(client);
+
+// discord-xp
+const Levels = require("discord-xp");
+
+Levels.setURL(process.env.database).then(console.log(chalk.greenBright('[info] Database connected for discord-xp.')));
+client.on("messageCreate", async (message) => {
+	if (!message.guild) return;
+	if (message.author.bot) return;
+
+	const randomAmountOfXp = Math.floor(Math.random() * 9) + 1;
+	const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+	if (hasLeveledUp) {
+		const user = await Levels.fetch(message.author.id, message.guild.id);
+		message.channel.send({ content: `Congratulations ${message.author.username}, your level is now ${user.level}. :tada:` })
+			.then(m => {
+				setTimeout(() => {
+					m.delete();
+				},
+				10000);
+			});
+
+	}
+});
+
 
 client.login(process.env.token);
