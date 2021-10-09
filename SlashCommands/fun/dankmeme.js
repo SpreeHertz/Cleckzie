1693704bc92eea070a89f53f85d924db3de245ac
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-10 00:00:41 +0530

    Fix dankmeme.js

--- SlashCommands/fun/dankmeme.js
+++ SlashCommands/fun/dankmeme.js
@@ -1,19 +1,19 @@
 const got = require('got');
 const { MessageEmbed } = require('discord.js');
 
+
 module.exports = {
 	name : 'dankmeme',
 	description: "Memes from r/dankmemes",
-	run: async (message, interaction) => {
+	run: async (client, interaction) => {
 		got('https://www.reddit.com/r/dankmemes/random/.json').then(res => {
 			const content = JSON.parse(res.body);
-			interaction.channel.send(
-				new MessageEmbed()
-					.setTitle(content[0].data.children[0].data.title)
-					.setImage(content[0].data.children[0].data.url)
-					.setColor("RANDOM")
-					.setFooter(`👍 ${content[0].data.children[0].data.ups} 👎 ${content[0].data.children[0].data.downs} | Comments: ${content[0].data.children[0].data.num_comments}`),
-			);
+			const embed = new MessageEmbed()
+				.setTitle(content[0].data.children[0].data.title)
+				.setImage(content[0].data.children[0].data.url)
+				.setColor("RANDOM")
+				.setFooter(`👍 ${content[0].data.children[0].data.ups} 👎 ${content[0].data.children[0].data.downs} | Comments: ${content[0].data.children[0].data.num_comments}`);
+			interaction.followUp({ embeds: [embed] });
 		});
 	},
 };
