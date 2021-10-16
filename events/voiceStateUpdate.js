ba82861b9584a6cf914c6f51c3d01f847d99f97a
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-16 18:26:03 +0530

    updaet

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,9 +1,6 @@
-const chalk = require('chalk');
 const client = require('..');
 const voiceClient = require('../Client/voiceClient');
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-	voiceClient.startListener(oldState, newState).then(
-		console.log('[info] -' + chalk.cyanBright(' Started listening for voiceStateUpdate. (djs-voice)')),
-	);
+	voiceClient.startListener(oldState, newState);
 });
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, Collection } = require("discord.js");
 require('dotenv').config();
-const chalk = require('chalk');
 
 // Note: 32767 means all intents.
 const client = new Client({
@@ -16,29 +15,6 @@ client.slashCommands = new Collection();
 client.config = require("./config/config.json");
 
 require("./handler")(client);
-
-// discord-xp
-const Levels = require("discord-xp");
-
-Levels.setURL(process.env.database).then(console.log(chalk.grey('[info] -' + chalk.cyanBright(' Database successfully connected') + chalk.magentaBright(' for messages leveling.') + chalk.yellow(' (discord-xp)'))));
-client.on("messageCreate", async (message) => {
-	if (!message.guild) return;
-	if (message.author.bot) return;
-
-	const randomAmountOfXp = Math.floor(Math.random() * 9) + 1;
-	const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
-	if (hasLeveledUp) {
-		const user = await Levels.fetch(message.author.id, message.guild.id);
-		message.channel.send({ content: `Congratulations ${message.author.username}, your level is now ${user.level}. :tada:` })
-			.then(m => {
-				setTimeout(() => {
-					m.delete();
-				},
-				10000);
-			});
-
-	}
-});
 
 
 client.login(process.env.token);
