a2affa99211640b51fe76832a973bb0451c06b3c
SpreeHertz <spreehertzofficial@gmail.com>
2021-12-11 23:50:48 +0530

    Add back files

--- events/onReady.js
+++ events/onReady.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
+const botActivity = require('../config.json').bot.activity;
+const botStatus = require('../config.json').bot.status;
 
 module.exports = {
 	name: "ready",
@@ -9,6 +11,7 @@ module.exports = {
 	 * @param {Object} client Main Application Client
 	 */
 	execute(client) {
+		client.user.setPresence({ activities: [{ name: botActivity }], status: botStatus });
 		console.log(chalk.cyanBright('success') + chalk.blue(` Ready! Logged in as ${client.user.tag}.`));
 	},
 };
